e24df1dfa0bc49c3ea430b80fd78efeb0a56d8a0
keita_kawaguchi <keikawagu@gmail.com>
2023-03-11 15:48:51 +0900

    コメント追加

--- object-design-solid/dip/good/Animal.ts
+++ object-design-solid/dip/good/Animal.ts
@@ -11,10 +11,3 @@ class Animal {
     this.animalRepository.findById()
   }
 }
-
-// badの例だと、AnimalクラスはAnimalRepositoryクラスに依存している。
-// 仮にユーザー検索をidではなくnameで行いたくなった時に、idをnameに修正するか、idに加えてnameも追加するかが必要
-// 前者の場合は都度修正が発生するし、後者の場合は凝集度が低くなる。
-
-// goodの例の場合は、Animalに注入するクラスを柔軟に変更できる。
-// nameで検索したくなってもAnimalRepositoryにnameを追加すれば柔軟に処理を変えられる。
